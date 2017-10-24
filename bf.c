ee92223eacb4994c6f1e022f3a19e97d21bb7293
David Markarian <udmsoft@outlook.com>
2017-10-24 12:58:51 +0000

    Made it smaller

--- bf.c
+++ bf.c
@@ -6,21 +6,20 @@ int y(char* a){
   while(*b!=0){
     b++;
   }
-  return (b-a);
+  return b-a;
 }
 //check for unbalanced brackets
 int z(char* a, int b){
-	int c = 0;
-	int d = 0;
+	int c=0,d=0;
 	while(c<b){
-		if(*(a+c)=='['){d++;}
-		if(*(a+c)==']'){d--;}
+		if(a[c]=='['){d++;}
+		if(a[c]==']'){d--;}
 		c++;
 	}
 	return d;
 }
 int main (int w, char* x[]){
-  puts("UDXS Business Card Brainf*** Interpreter @ udxs.me\n");
+  puts("UDXS Biz Card Brainf*** Interpreter @ udxs.me\n");
   if(w!=3){puts("Missing Args!\n");return 0;}
   //program size
   int a=y(x[1]);
@@ -29,23 +28,14 @@ int main (int w, char* x[]){
   int b=y(x[2]);
   //stack
   short c[256]={0};
-  //stack pos
-  char d=0;
-  //current program char
-  short e=0;
-  //current input char
-  short f=0;
-  //cells
+  //stack pos, current program char, current input char, required depth
+  char d=0,e=0,f=0,i=0,j=0;
   char* g = calloc(1<<sizeof(short)*8,1);
   //current cell pos
   unsigned short h = 0;
-  //required depth
-  char i = 0;
-  //current depth
-  char j = 0;
   while(e<a) {
-		char k = *(x[1]+e);
-		char l = *(x[2]+f);
+		char k = x[1][e];
+		char l = x[2][f];
     if(i!=j){
       if(k=='['){j++;}
       if(k==']'){j--;}
@@ -53,9 +43,9 @@ int main (int w, char* x[]){
       continue;
     }
     if(k=='['&&g[h]){i++;j++;d++;c[d]=e;}
-    if(k=='['&&g[h]==0){j++;}
+    if(k=='['&&!g[h]){j++;}
     if(k==']'&&g[h]){e=c[d];}
-    if(k==']'&&g[h]==0){d--;j--;i--;}
+    if(k==']'&&!g[h]){d--;j--;i--;}
     if(k=='+'){g[h]++;}
     if(k=='-'){g[h]--;}
     if(k=='>'){h++;}
