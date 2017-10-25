1d7cb18c586a7a1e1a50ccceef6fbdd08c03d797
David Markarian <udmsoft@outlook.com>
2017-10-25 18:21:57 +0000

    Fixed issue where programs couldn't be larger than 256 chars and made code smaller

--- bf.c
+++ bf.c
@@ -21,15 +21,13 @@ int z(char* a, int b){
 int main (int w, char* x[]){
   puts("UDXS Biz Card Brainf*** Interpreter @ udxs.me\n");
   if(w!=3){puts("Missing Args!\n");return 0;}
-  //program size
-  int a=y(x[1]);
+  //program size ,input size
+  int a=y(x[1]),b=y(x[2]);
   if(z(x[1],a)){puts("Unbalanced Brackets!\n");return 0;}
-  //input size
-  int b=y(x[2]);
   //stack
-  short c[256]={0};
+  short c[256]={0},e=0,f=0;
   //stack pos, current program char, current input char, required depth
-  char d=0,e=0,f=0,i=0,j=0;
+  char d=0,i=0,j=0;
   char* g = calloc(1<<sizeof(short)*8,1);
   //current cell pos
   unsigned short h = 0;
@@ -46,7 +44,7 @@ int main (int w, char* x[]){
     if(k=='['&&!g[h]){j++;}
     if(k==']'&&g[h]){e=c[d];}
     if(k==']'&&!g[h]){d--;j--;i--;}
-    if(k=='+'){g[h]++;}
+    if(k=='+'){g[h]++;} 
     if(k=='-'){g[h]--;}
     if(k=='>'){h++;}
     if(k=='<'){h--;}
